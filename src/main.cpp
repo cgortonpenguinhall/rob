8659395eed5503be519bb0222a9935361820d658
Megan Lempke <megan@Megans-MacBook-Air-2.local>
2020-04-28 11:33:56 -0400

    right arm wave

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   leftArm.write(45);
   delay(1000);
-  leftArm.write(75);
+  leftArm.write(120);
   delay(1000);
-
+  rightArm.write(45);
+  delay(1000);
+  rightArm.write(120);
+  delay(1000);
 }
+

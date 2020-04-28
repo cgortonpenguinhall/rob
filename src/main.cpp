40c5c2eb2007e6074d8a9f26ae017cc92e2bbf03
Megan Lempke <megan@Megans-MacBook-Air-2.local>
2020-04-28 11:28:54 -0400

    left arm wave

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <Arduino.h>
-
+#include <Servo.h>
 #include <someFunctions.h>
+
+Servo leftArm;
+Servo rightArm;
 
 void setup() {
   Serial.begin(9600);
+  leftArm.attach(12); 
+  rightArm.attach(13);
   someFunction();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  leftArm.write(45);
+  delay(1000);
+  leftArm.write(75);
+  delay(1000);
+
 }
-
-//whatever you want to say megan 
-
-//today is april 15th

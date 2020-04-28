b2d079558dfef7f888a267433180512b5de448bb
Megan Lempke <megan@Megans-MacBook-Air-2.local>
2020-04-28 11:56:58 -0400

    head function and fixed hands

---
+++ include/head.h
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+
+void moveHead (){ 
+    head.write(0);
+    delay(1000);
+    head.write(180);
+    delay(1000);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
 #include <Servo.h>
-#include <someFunctions.h>
 
 const int leftArmPin = 12;  //blue
 const int rightArmPin = 13; // yellow
+const int headPin = 11; // green
 
 Servo leftArm;
 Servo rightArm;
+Servo head; 
+
+#include <hands.h>
+#include <head.h>
 
 void setup() {
   Serial.begin(9600);
   leftArm.attach(leftArmPin);
   rightArm.attach(rightArmPin);
+  head.attach(headPin);
   waveHands();
+  moveHead();
 }
 
 void loop() {

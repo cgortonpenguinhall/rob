a46b3fa8e0f43ff4b0ae2709a877b9f84757ff9c
Craig Gorton <cgorton@penguinhall.org>
2020-04-30 13:54:29 -0400

    get ultrasonic working
    Co-authored-by: ssoper20 <ssoper20@penguinhall.org>
    Co-authored-by: mlempke20 <mlempke20@penguinhall.org>

--- include/head.h
+++ include/head.h
@@ -1,6 +1,8 @@
-void moveHead (){ 
-    head.write(0);
-    delay(1000);
-    head.write(180);
-    delay(1000);
+void moveHead() {
+  head.write(0);
+  delay(1000);
+  head.write(180);
+  delay(1000);
+  head.write(90);
+  delay(1000);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <NewPing.h>
 
-const int leftArmPin = 12;  //blue
+const int leftArmPin = 12;  // blue
 const int rightArmPin = 13; // yellow
 const int headPin = 11; // green
+const int echoPin = 8; //blue 
+const int triggerPin = 9; //yellow 
+const int maxDistance = 200; 
 
 Servo leftArm;
 Servo rightArm;
-Servo head; 
+Servo head;
+
+NewPing sonar (triggerPin, echoPin, maxDistance);
 
 #include <hands.h>
 #include <head.h>
@@ -17,11 +23,17 @@ void setup() {
   leftArm.attach(leftArmPin);
   rightArm.attach(rightArmPin);
   head.attach(headPin);
-  waveHands();
-  moveHead();
 }
 
 void loop() {
-
+  int dist = sonar.ping_cm();
+  Serial.println(dist);
+  delay(100);
+    if (dist<=75 && dist>0){
+      Serial.println(dist);
+      waveHands();
+      moveHead();  
+      //fiberoptics 
+    }
 
 }
